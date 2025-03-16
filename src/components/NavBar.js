ea3237ae1c5e6a93b49b1719077bfafcb5d413db
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-17 00:01:58 +0100

    Add function to close NavBar when clicking on a link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,12 @@ import { NavLink } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import useClickOutside from '../hooks/useClickOutside';
 
 const NavBar = () => {
+
+    const { expanded, setExpanded, ref } = useClickOutside();
+
     return (
         <>
             <Navbar collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
@@ -12,8 +16,8 @@ const NavBar = () => {
                     <NavLink to="/">
                         <Navbar.Brand href="#home">Hack Collect</Navbar.Brand>
                     </NavLink>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse>
                         <Nav className="ms-auto">
                             <Nav.Link href="#">Home</Nav.Link>
                             <Nav.Link href="#">Hacks</Nav.Link>
