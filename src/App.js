f57ae61cd1694174953e695126c7c5e1779de7e8
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-16 23:46:11 +0100

    Add content container and router to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <>
-        <NavBar />
-      </>
+      <NavBar />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={() => { }} />
+          <Route render={() => <h1>Page not found!</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
