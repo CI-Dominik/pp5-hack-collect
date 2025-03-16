a057bd7e186a056b2d9144543b3bbf5167d0807b
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-16 23:57:18 +0100

    Add logo link to homepage and useClickOutside to close menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,7 +9,9 @@ const NavBar = () => {
         <>
             <Navbar collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="#home">Hack Collect</Navbar.Brand>
+                    <NavLink to="/">
+                        <Navbar.Brand href="#home">Hack Collect</Navbar.Brand>
+                    </NavLink>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="ms-auto">
