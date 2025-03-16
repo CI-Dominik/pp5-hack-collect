4c6cfe1bd71ad62273c4cbde5dc068e0a8b778bb
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-16 22:18:06 +0100

    Adjust NavBar to show only two links and add collapsable

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,14 +6,16 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar = () => {
     return (
         <>
-            <Navbar bg="dark" data-bs-theme="dark">
+            <Navbar collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
+                    <Navbar.Brand href="#home">Hack Collect</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#">Home</Nav.Link>
+                            <Nav.Link href="#">Hacks</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
