3ada54acab5c623245ba4b94a13cb12f56cb0144
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-03-22 01:21:56 +0100

    Add NavBar links and CSS file

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,58 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import useClickOutside from '../hooks/useClickOutside';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
 
     const { expanded, setExpanded, ref } = useClickOutside();
+
+
+    const loggedInLinks = (
+        <>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/followed">
+                <i className="fas fa-stream"></i> Followed
+            </NavLink>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/liked">
+                <i className="fas fa-heart"></i> Liked
+            </NavLink>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                to="/"
+                onClick={() => {}}
+            >
+                <i className="fas fa-sign-out-alt"></i> Sign-out
+            </NavLink>
+        </>
+    );
+
+    const loggedOutLinks = (
+        <>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/sign-in">
+                <i className="fas fa-sign-in-alt"></i> Sign-in
+            </NavLink>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/sign-up">
+                <i className="fas fa-user-plus"></i> Sign-up
+            </NavLink>
+        </>
+    );
 
     return (
         <>
