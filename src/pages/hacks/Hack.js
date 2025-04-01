98cd06e6d1ab4bff565458e26c5502a19e4acc54
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-01 08:49:33 +0200

    Add profile_information to Hack component and link to Hack page

--- src/pages/hacks/Hack.js
+++ src/pages/hacks/Hack.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../../styles/Hack.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { axiosRes } from "../../api/axiosDefaults";
 import ActionDropdown from "../../components/ActionDropdown";
 
@@ -16,6 +16,8 @@ const Hack = (props) => {
     updated_at,
     created_at,
     category,
+    profile_id,
+    profile_image,
   } = props;
 
   const currentUser = useCurrentUser();
@@ -39,7 +41,9 @@ const Hack = (props) => {
     <Card className={styles.Hack}>
       <Card.Header>
         <Media className="align-items-center">
-          <img src={image} alt={title} className={styles.HackImage} />
+          <Link to={`/hacks/${id}`}>
+            <img src={image} alt={title} className={styles.HackImage} />
+          </Link>
           <div className="d-flex align-items-center">
             <span>
               {updated_at} ({created_at})
@@ -53,7 +57,9 @@ const Hack = (props) => {
         </Media>
       </Card.Header>
       <Card.Body>
-        <Card.Title className="text-center">{title}</Card.Title>
+        <Link to={`/hacks/${id}`}>
+          <Card.Title className="text-center">{title}</Card.Title>
+        </Link>
         <Card.Subtitle className="text-muted">{category}</Card.Subtitle>
         <Card.Text>{content}</Card.Text>
       </Card.Body>
