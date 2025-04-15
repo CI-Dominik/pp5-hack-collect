48bad94f6035fde62d8c65fdf922dace333654f9
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-15 04:10:48 +0200

    Add context to fetch profiles and add Provider to index tree

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <CurrentUserProvider>
-        <App />
-      </CurrentUserProvider>
+      <ProfileDataProvider>
+        <CurrentUserProvider>
+          <App />
+        </CurrentUserProvider>
+      </ProfileDataProvider>
     </Router>
   </React.StrictMode>
 );
