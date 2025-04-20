cdd756248b31be6609f101c998d9f9864f04d41f
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-20 23:53:04 +0200

    Remove sticky styling from NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -77,7 +77,7 @@ const NavBar = () => {
 
     return (
         <>
-            <Navbar collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary p-2 sticky-top text-white">
+            <Navbar collapseOnSelect bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary p-2 text-white">
                 <NavLink className="text-decoration-none" to="/">
                     <Navbar.Brand className={styles.Title}>Hack Collect</Navbar.Brand>
                 </NavLink>
