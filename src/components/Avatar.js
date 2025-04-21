d07a0d22a0e6e5cf130d5ed23885255494b632eb
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-04-21 17:11:57 +0200

    Adjust margin for follow button on profile and styling for Avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,9 +3,7 @@ import styles from '../styles/Avatar.module.css'
 
 const Avatar = ({ src, height = 45, text }) => {
     return (
-        <div className="d-flex flex-row align-items-center p-0">
             <span className="fw-bold"><img className={styles.Avatar} src={src} height={height} width={height} alt="Avatar" /> {text}</span>
-        </div>
     )
 }
 
